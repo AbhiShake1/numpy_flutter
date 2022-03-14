800227c45f495e96b8c44cca2f471c76d8577530
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2022-03-14 22:54:43 +0545

    test: refactor unit tests

--- test/numpy_flutter_test.dart
+++ test/numpy_flutter_test.dart
@@ -4,43 +4,44 @@ import 'package:numpy_flutter/numpy_flutter.dart';
 
 void main() {
   test('transpose of matrix', () {
-    List<List> array;
-    List<List> transposeArray;
-    array = [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9],
+    List<List<List>> arrays = [
+      [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+      ],
+      [
+        [1, 2, 3],
+        [4, 5, 6],
+      ],
+      [[]],
+      [],
+      [
+        [1, 2, 3]
+      ],
     ];
-    transposeArray = [
-      [1, 4, 7],
-      [2, 5, 8],
-      [3, 6, 9],
+    List<List<List>> transposeArrays = [
+      [
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+      ],
+      [
+        [1, 4],
+        [2, 5],
+        [3, 6],
+      ],
+      [[]],
+      [],
+      [
+        [1],
+        [2],
+        [3],
+      ],
     ];
-    expect(array.transpose(), transposeArray);
-    array = [
-      [1, 2, 3],
-      [4, 5, 6],
-    ];
-    transposeArray = [
-      [1, 4],
-      [2, 5],
-      [3, 6],
-    ];
-    expect(array.transpose(), transposeArray);
-    array = [[]];
-    transposeArray = [[]];
-    expect(array.transpose(), transposeArray);
-    array = [];
-    transposeArray = [];
-    expect(array.transpose(), transposeArray);
-    array = [
-      [1, 2, 3]
-    ];
-    transposeArray = [
-      [1],
-      [2],
-      [3],
-    ];
-    expect(array.transpose(), transposeArray);
+    //since we ensure there are same no. of outer arrays
+    for (int i = 0; i < arrays.length; i++) {
+      expect(arrays[i].transpose(), transposeArrays[i]);
+    }
   });
 }
