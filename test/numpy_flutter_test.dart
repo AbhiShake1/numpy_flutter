94648c16d45a6d690a185749c6e4ced230f74bc6
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2022-03-14 22:32:56 +0545

    test: write unit tests for transpose

--- test/numpy_flutter_test.dart
+++ test/numpy_flutter_test.dart
@@ -3,10 +3,44 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:numpy_flutter/numpy_flutter.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('transpose of matrix', () {
+    List<List> array;
+    List<List> transposeArray;
+    array = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+    transposeArray = [
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+    ];
+    expect(array.transpose(), transposeArray);
+    array = [
+      [1, 2, 3],
+      [4, 5, 6],
+    ];
+    transposeArray = [
+      [1, 4],
+      [2, 5],
+      [3, 6],
+    ];
+    expect(array.transpose(), transposeArray);
+    array = [[]];
+    transposeArray = [[]];
+    expect(array.transpose(), transposeArray);
+    array = [];
+    transposeArray = [];
+    expect(array.transpose(), transposeArray);
+    array = [
+      [1, 2, 3]
+    ];
+    transposeArray = [
+      [1],
+      [2],
+      [3],
+    ];
+    expect(array.transpose(), transposeArray);
   });
 }
